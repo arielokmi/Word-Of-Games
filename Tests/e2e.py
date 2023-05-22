509f7049dd88b40ee4f33a59a5e05ae860c6c8a0
ariel <arielokmi@gmial.com>
2023-05-22 15:12:23 +0300

    remove chromedriver.exe

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_scores_service(web_url="http://127.0.0.1:5000/"):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(version='113.0.5672.63').install()))
     driver.get(web_url)
     score = driver.find_element(By.ID, "score").text
     score = int(score)
